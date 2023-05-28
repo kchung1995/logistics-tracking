4edd4bbd87a67496b4bba7de32ec7ee9b42393a2
Kuyho Chung <kchung1995@gmail.com>
2023-05-28 18:21:43 +0900

    feat: added response object for tracker API - TrackerDeliveryResponse

---
+++ src/main/kotlin/com/tistory/katfun/logistics/models/TrackerDeliveryResponse.kt
@@ -0,0 +1,44 @@
+package com.tistory.katfun.logistics.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TrackerDeliveryResponse(
+    val from: TrackerDeliveryPerson,
+    val to: TrackerDeliveryPerson,
+    val state: TrackerDeliveryStatus,
+    val progress: List<TrackerDeliveryProgress>,
+    val carrier: TrackerDeliveryCarrier,
+)
+
+@Serializable
+data class TrackerDeliveryPerson(
+    val name: String,
+    val time: String,
+)
+
+@Serializable
+data class TrackerDeliveryStatus(
+    val id: String,
+    val text: String,
+)
+
+@Serializable
+data class TrackerDeliveryProgress(
+    val time: String,
+    val status: TrackerDeliveryStatus,
+    val location: TrackerDeliveryLocation,
+    val description: String,
+)
+
+@Serializable
+data class TrackerDeliveryLocation(
+    val name: String,
+)
+
+@Serializable
+data class TrackerDeliveryCarrier(
+    val id: String,
+    val name: String,
+    val tel: String,
+)
