a72b229dedc0433d9415c986df0eab23b3268132
Kuyho Chung <kchung1995@gmail.com>
2023-05-28 17:11:51 +0900

    feat: separated run configuration to application.conf

--- src/main/kotlin/com/tistory/katfun/Application.kt
+++ src/main/kotlin/com/tistory/katfun/Application.kt
@@ -1,14 +1,11 @@
 package com.tistory.katfun
 
+import com.tistory.katfun.plugins.configureRouting
+import com.tistory.katfun.plugins.configureSerialization
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.tistory.katfun.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     configureSerialization()
