d2a14aa0e089dd4cc00d8c42cdea7d8d437f5ca4
Kuyho Chung <kchung1995@gmail.com>
2023-05-29 15:04:12 +0900

    feat: added LocalDateTimeSerailzer

---
+++ src/main/kotlin/com/tistory/katfun/utilities/LocalDateTimeSerializer.kt
@@ -0,0 +1,28 @@
+package com.tistory.katfun.utilities
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Serializer(forClass = LocalDateTime::class)
+object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+    private val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        val formatted = formatter.format(value)
+        encoder.encodeString(formatted)
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        val formatted = decoder.decodeString()
+        return LocalDateTime.parse(formatted, formatter)
+    }
+}
